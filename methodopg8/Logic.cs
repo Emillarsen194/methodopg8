f199fadb435efa4e39d67591d4ebf20d76265f63
Emillarsen194 <47022984+Emillarsen194@users.noreply.github.com>
2019-02-17 15:04:26 +0100

    Add project files.

---
+++ methodopg8/Logic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodopg8
+{
+    class Logic
+    {
+        public static List<int> Removenumbers3goesinto(List<int> liste)
+        {
+            for (int i = 0; i < liste.Count; i++) // we use count with list to see the length of the list 
+            {
+
+                if (liste[i] % 3 == 0) // if one of our numbers in our list modulus 3 is the same as 0 3 goes up in the number
+                {
+                   liste.Remove(liste[i]); // then remove the numbers we found
+                }
+            }
+
+            return liste;
+        }
+
+        public static List<int> InsertANumberToList(List<int> liste, int index, int number) // insert a number on which index u choose
+        {
+            liste.Insert(index, number);
+
+            return liste;
+        }
+
+        public static List<int> ReverseNumber(List<int> liste) //reversing numbers in a list
+        {
+            liste.Reverse();
+
+            return liste;
+        }
+
+        public static List<int> PrintListGui(List<int> Liste) //printing the list u choose
+        {
+            foreach (int item in Liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            return Liste;
+        }
+
+           
+    }
+}
---
+++ methodopg8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodopg8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listeB = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+
+
+          
+            //list b 
+            Logic.Removenumbers3goesinto(listeB); // removes all numbers 3 goes into
+            Logic.InsertANumberToList(listeB, 2, 17); //insert number 17 to index 3 in list
+            Logic.PrintListGui(listeB); // printing our list
+
+
+
+            //reversed numbers list
+            List<int> reversedNumbers = new List<int>(listeB); // copy old lists numbers so we can reverse it
+
+            Logic.ReverseNumber(reversedNumbers); //reversing the numbers of list reversednumbers
+
+            Logic.PrintListGui(reversedNumbers); // printing our list
+
+
+
+        }
+    }
+}
